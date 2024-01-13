e9142631171ab8d9c507fa20507ee37b16a29cc9
Mahmut Ali Şahinkaya <mahmutalisahinkaya88@gmail.com>
2024-01-13 11:55:12 +0300

    order-service updated

--- order-service/src/main/java/com/ecommerce/orderservice/controller/CartController.java
+++ order-service/src/main/java/com/ecommerce/orderservice/controller/CartController.java
@@ -4,7 +4,6 @@ import com.ecommerce.orderservice.dto.CartItemDto;
 import com.ecommerce.orderservice.service.CartService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,14 +17,12 @@ public class CartController {
     }
 
     @PostMapping("/items")
-    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Void> addItemToCart(@RequestBody CartItemDto cartItemDto) {
         cartService.addItemToCart(cartItemDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/items/{cartItemId}")
-    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<CartItemDto> updateCartItem(@PathVariable Long cartItemId,
                                                       @RequestBody CartItemDto cartItemDto) {
         CartItemDto updatedCartItem = cartService.updateCartItem(cartItemId, cartItemDto);
@@ -33,7 +30,6 @@ public class CartController {
     }
 
     @DeleteMapping("/items/{cartItemId}")
-    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Void> removeItemFromCart(@PathVariable Long cartItemId) {
         cartService.removeItemFromCart(cartItemId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
